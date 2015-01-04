1ffdc5175c0c4628638e11d077b8be50160e12b1
Victor <melnikukr@mail.ru>
2015-01-04 20:34:12 +0200

    Set Objects class to final

--- src/com/annimon/stream/Objects.java
+++ src/com/annimon/stream/Objects.java
@@ -6,7 +6,7 @@ import java.util.Comparator;
  * Common operations with Object.
  * @author aNNiMON
  */
-public  class Objects {
+public final class Objects {
     
     public static boolean equals(Object a, Object b) {
         return (a == b) || (a != null && a.equals(b));
