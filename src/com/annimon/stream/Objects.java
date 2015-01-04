7581cb79f34a08edd6db1eb98ff0475a0ef16b22
Victor <melnikukr@mail.ru>
2015-01-04 17:44:43 +0200

    Add Objects class

---
+++ src/com/annimon/stream/Objects.java
@@ -0,0 +1,38 @@
+package com.annimon.stream;
+
+import java.util.Comparator;
+
+/**
+ * Common operations with Object.
+ * @author aNNiMON
+ */
+public  class Objects {
+    
+    public static boolean equals(Object a, Object b) {
+        return (a == b) || (a != null && a.equals(b));
+    }
+    
+    public static int hashCode(Object o) {
+        return o != null ? o.hashCode() : 0;
+    }
+    
+    public static String toString(Object o, String nullDefault) {
+        return (o != null) ? o.toString() : nullDefault;
+    }
+    
+    public static <T> int compare(T a, T b, Comparator<? super T> c) {
+        return (a == b) ? 0 : c.compare(a, b);
+    }
+    
+    public static <T> T requireNonNull(T obj) {
+        if (obj == null)
+            throw new NullPointerException();
+        return obj;
+    }
+    
+    public static <T> T requireNonNull(T obj, String message) {
+        if (obj == null)
+            throw new NullPointerException(message);
+        return obj;
+    }
+}
