0ab5635255ee0b03b6000299ceed384c42923b67
Florian Barth <solderra@gmail.com>
2016-03-06 02:39:00 +0100

    fix imports

--- src/main/java/com/annimon/stream/Stream.java
+++ src/main/java/com/annimon/stream/Stream.java
@@ -7,8 +7,10 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Queue;
 import java.util.Set;
 
 /**
