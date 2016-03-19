3169091e3556a052384eb667673e217876c4f2c9
Victor <melnikukr@mail.ru>
2016-03-19 02:28:22 +0200

    Update tests

--- streamTest/src/test/java/com/annimon/stream/test/OptionalMatcherTest.java
+++ streamTest/src/test/java/com/annimon/stream/test/OptionalMatcherTest.java
@@ -4,16 +4,27 @@ import com.annimon.stream.Optional;
 import static com.annimon.stream.test.OptionalMatcher.isEmpty;
 import static com.annimon.stream.test.OptionalMatcher.isPresent;
 import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.CoreMatchers.is;
+import org.hamcrest.StringDescription;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class OptionalMatcherTest {
 
     @Test
+    public void testPrivateConstructor() throws Exception {
+        TestUtils.testPrivateConstructor(OptionalMatcher.class);
+    }
+
+    @Test
     public void testIsPresent() {
         Optional<Integer> optional = Optional.of(5);
         assertThat(optional, isPresent());
         assertThat(optional, not(isEmpty()));
+
+        StringDescription description = new StringDescription();
+        isPresent().describeTo(description);
+        assertThat(description.toString(), is("Optional value should be present"));
     }
 
     @Test
--- streamTest/src/test/java/com/annimon/stream/test/StreamMatcherTest.java
+++ streamTest/src/test/java/com/annimon/stream/test/StreamMatcherTest.java
@@ -6,22 +6,46 @@ import static com.annimon.stream.test.StreamMatcher.isEmpty;
 import static com.annimon.stream.test.StreamMatcher.isNotEmpty;
 import java.util.Arrays;
 import java.util.List;
+import static org.hamcrest.CoreMatchers.allOf;
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.is;
+import org.hamcrest.Matcher;
+import org.hamcrest.StringDescription;
+import org.hamcrest.core.IsNot;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class StreamMatcherTest {
 
     @Test
+    public void testPrivateConstructor() throws Exception {
+        TestUtils.testPrivateConstructor(StreamMatcher.class);
+    }
+
+    @Test
     public void testIsEmpty() {
         assertThat(Stream.empty(), isEmpty());
         assertThat(Stream.of(1, 2), isNotEmpty());
+
+        StringDescription description = new StringDescription();
+        isEmpty().describeTo(description);
+        assertThat(description.toString(), is("an empty stream"));
     }
 
     @Test
     public void testElements() {
         final Stream<Integer> stream = Stream.range(0, 5);
         final List<Integer> expected = Arrays.asList(0, 1, 2, 3, 4);
-        assertThat(stream, elements(is(expected)));
+        final Matcher<Stream<Integer>> matcher = elements(is(expected));
+        assertThat(stream, matcher);
+
+        assertFalse(elements(is(expected)).matches(Stream.<Integer>empty()));
+
+        StringDescription description = new StringDescription();
+        matcher.describeTo(description);
+        assertThat(description.toString(), allOf(
+                containsString("Stream elements is"),
+                containsString(expected.toString())
+        ));
     }
 }
