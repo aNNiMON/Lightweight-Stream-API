1316b1dc592b856f2191fb6c8d5e74b9d4f15458
Pavel Sveda <xsveda@gmail.com>
2016-06-26 17:50:07 +0200

    Add Mockito argument matcher for Optional

---
+++ streamTest/src/main/java/com/annimon/stream/test/mockito/OptionalMatcher.java
@@ -0,0 +1,52 @@
+package com.annimon.stream.test.mockito;
+
+import com.annimon.stream.Optional;
+
+import org.mockito.ArgumentMatcher;
+
+import static org.mockito.Matchers.argThat;
+
+public class OptionalMatcher {
+
+    public static <T> Optional<T> anyPresentOptional() {
+        return argThat(new PresentOptionalMatcher<T>());
+    }
+
+    public static <T> Optional<T> anyPresentOptional(@SuppressWarnings("UnusedParameters") Class<T> clazz) {
+        return argThat(new PresentOptionalMatcher<T>());
+    }
+
+    public static <T> Optional<T> anyEmptyOptional() {
+        return argThat(new EmptyOptionalMatcher<T>());
+    }
+
+    public static <T> Optional<T> anyEmptyOptional(@SuppressWarnings("UnusedParameters") Class<T> clazz) {
+        return argThat(new EmptyOptionalMatcher<T>());
+    }
+
+    public static class PresentOptionalMatcher<T> implements ArgumentMatcher<Optional<T>> {
+
+        @Override
+        public boolean matches(Optional<T> argument) {
+            return argument != null && argument.isPresent();
+        }
+
+        @Override
+        public String toString() {
+            return "anyPresentOptional()";
+        }
+    }
+
+    public static class EmptyOptionalMatcher<T> implements ArgumentMatcher<Optional<T>> {
+
+        @Override
+        public boolean matches(Optional<T> argument) {
+            return argument != null && !argument.isPresent();
+        }
+
+        @Override
+        public String toString() {
+            return "anyEmptyOptional()";
+        }
+    }
+}
