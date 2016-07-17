086a4f3d4208cd21a7cfa4b960d757d23665ff94
Victor <melnikukr@mail.ru>
2016-07-17 11:48:38 +0300

    Add ObjIntConsumer function

---
+++ stream/src/main/java/com/annimon/stream/function/ObjIntConsumer.java
@@ -0,0 +1,19 @@
+package com.annimon.stream.function;
+
+/**
+ * Represents an operation on two input arguments.
+ *
+ * @param <T> the type of the first argument
+ * @see BiConsumer
+ */
+@FunctionalInterface
+public interface ObjIntConsumer<T> {
+
+    /**
+     * Performs operation on two arguments.
+     *
+     * @param t  the first argument
+     * @param value  the second argument
+     */
+    void accept(T t, int value);
+}
