1e24d3e7ba7f89d60052936f44eee031aba254aa
Pawel Kwiecinski <pawel.kwiecinski@allegrogroup.com>
2016-08-10 15:47:29 +0200

    Revert to non-star import style.

--- stream/src/main/java/com/annimon/stream/Stream.java
+++ stream/src/main/java/com/annimon/stream/Stream.java
@@ -2,7 +2,16 @@ package com.annimon.stream;
 
 import com.annimon.stream.function.*;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedHashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.Queue;
+import java.util.Set;
 
 /**
  * A sequence of elements supporting aggregate operations.
