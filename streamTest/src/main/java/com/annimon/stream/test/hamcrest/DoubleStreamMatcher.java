96b22eb29ab5aabb696a4781d692e1ee2d39bdcb
Victor <annimon119@gmail.com>
2016-11-19 11:24:26 +0200

    Add hamcrest matchers for double

---
+++ streamTest/src/main/java/com/annimon/stream/test/hamcrest/DoubleStreamMatcher.java
@@ -0,0 +1,91 @@
+package com.annimon.stream.test.hamcrest;
+
+import com.annimon.stream.DoubleStream;
+import com.annimon.stream.function.IntFunction;
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeDiagnosingMatcher;
+
+public class DoubleStreamMatcher {
+
+    private DoubleStreamMatcher() { }
+
+    public static Matcher<DoubleStream> isEmpty() {
+        return new IsEmptyMatcher();
+    }
+
+    public static Matcher<DoubleStream> hasElements() {
+        return new HasElementsMatcher();
+    }
+
+    public static Matcher<DoubleStream> elements(Matcher<Double[]> matcher) {
+        return new ElementsMatcher(matcher);
+    }
+
+    public static class IsEmptyMatcher extends TypeSafeDiagnosingMatcher<DoubleStream> {
+
+        @Override
+        protected boolean matchesSafely(DoubleStream stream, Description mismatchDescription) {
+            mismatchDescription.appendText("DoubleStream was not empty");
+            return stream.count() == 0;
+        }
+
+        @Override
+        public void describeTo(Description description) {
+            description.appendText("an empty stream");
+        }
+    }
+
+    public static class HasElementsMatcher extends TypeSafeDiagnosingMatcher<DoubleStream> {
+
+        @Override
+        protected boolean matchesSafely(DoubleStream stream, Description mismatchDescription) {
+            mismatchDescription.appendText("DoubleStream was empty");
+            return stream.count() > 0;
+        }
+
+        @Override
+        public void describeTo(Description description) {
+            description.appendText("a non-empty stream");
+        }
+    }
+
+    public static class ElementsMatcher extends TypeSafeDiagnosingMatcher<DoubleStream> {
+
+        private final Matcher<Double[]> matcher;
+        private Double[] streamElements;
+
+        public ElementsMatcher(Matcher<Double[]> matcher) {
+            this.matcher = matcher;
+        }
+
+        @Override
+        protected boolean matchesSafely(DoubleStream stream, Description mismatchDescription) {
+            final Double[] elements;
+            if (streamElements == null) {
+                elements = stream.boxed().toArray(BOXED_ARRAY_CREATOR);
+                streamElements = elements;
+            } else {
+                elements = streamElements;
+            }
+            if (!matcher.matches(elements)) {
+                mismatchDescription.appendText("DoubleStream elements ");
+                matcher.describeMismatch(elements, mismatchDescription);
+                return false;
+            }
+            return true;
+        }
+
+        @Override
+        public void describeTo(Description description) {
+            description.appendText("DoubleStream elements ").appendDescriptionOf(matcher);
+        }
+    }
+
+    private static final IntFunction<Double[]> BOXED_ARRAY_CREATOR = new IntFunction<Double[]>() {
+        @Override
+        public Double[] apply(int value) {
+            return new Double[value];
+        }
+    };
+}
---
+++ streamTest/src/main/java/com/annimon/stream/test/hamcrest/OptionalDoubleMatcher.java
@@ -0,0 +1,82 @@
+package com.annimon.stream.test.hamcrest;
+
+import com.annimon.stream.OptionalDouble;
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeDiagnosingMatcher;
+import static org.hamcrest.CoreMatchers.is;
+
+public class OptionalDoubleMatcher {
+
+    private OptionalDoubleMatcher() { }
+
+    public static Matcher<OptionalDouble> isPresent() {
+        return new IsPresentMatcher();
+    }
+
+    public static Matcher<OptionalDouble> isEmpty() {
+        return new IsEmptyMatcher();
+    }
+
+    public static <T> Matcher<OptionalDouble> hasValue(double value) {
+        return hasValueThat(is(value));
+    }
+
+    public static Matcher<OptionalDouble> hasValueThat(Matcher<? super Double> matcher) {
+        return new HasValueMatcher(matcher);
+    }
+
+    public static class IsPresentMatcher extends TypeSafeDiagnosingMatcher<OptionalDouble> {
+
+        @Override
+        protected boolean matchesSafely(OptionalDouble optional, Description mismatchDescription) {
+            mismatchDescription.appendText("OptionalDouble was empty");
+            return optional.isPresent();
+        }
+
+        @Override
+        public void describeTo(Description description) {
+            description.appendText("OptionalDouble value should be present");
+        }
+    }
+
+    public static class IsEmptyMatcher extends TypeSafeDiagnosingMatcher<OptionalDouble> {
+
+        @Override
+        protected boolean matchesSafely(OptionalDouble optional, Description mismatchDescription) {
+            mismatchDescription.appendText("OptionalDouble was present");
+            return !optional.isPresent();
+        }
+
+        @Override
+        public void describeTo(Description description) {
+            description.appendText("OptionalDouble value should be empty");
+        }
+    }
+
+    public static class HasValueMatcher extends TypeSafeDiagnosingMatcher<OptionalDouble> {
+
+        private final Matcher<? super Double> matcher;
+
+        public HasValueMatcher(Matcher<? super Double> matcher) {
+            this.matcher = matcher;
+        }
+
+        @Override
+        protected boolean matchesSafely(OptionalDouble optional, Description mismatchDescription) {
+            if (!optional.isPresent()) {
+                mismatchDescription.appendText("OptionalDouble was empty");
+                return false;
+            }
+            final Double value = optional.getAsDouble();
+            mismatchDescription.appendText("OptionalDouble value ");
+            matcher.describeMismatch(value, mismatchDescription);
+            return matcher.matches(value);
+        }
+
+        @Override
+        public void describeTo(Description description) {
+            description.appendText("OptionalDouble value ").appendDescriptionOf(matcher);
+        }
+    }
+}
