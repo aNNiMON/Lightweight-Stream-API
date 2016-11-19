f8475af6f29280ba646c9181093aca3f1fb8364e
Victor <annimon119@gmail.com>
2016-11-19 14:55:24 +0200

    Add RandomCompat doubles methods

--- stream/src/main/java/com/annimon/stream/RandomCompat.java
+++ stream/src/main/java/com/annimon/stream/RandomCompat.java
@@ -1,12 +1,11 @@
 package com.annimon.stream;
 
+import com.annimon.stream.function.DoubleSupplier;
 import com.annimon.stream.function.IntSupplier;
-
 import java.util.Random;
 
 /**
- * Backported stream apis from {@link java.util.Random} class,
- * based on {@link IntStream} only for now.
+ * Backported stream apis from {@link java.util.Random} class.
  */
 @SuppressWarnings("WeakerAccess")
 public final class RandomCompat {
@@ -23,7 +22,7 @@ public final class RandomCompat {
     /**
      * Constructs object, inner {@code random} created with seed passed as param.
      *
-     * @param seed seed to initialize {@code random} object
+     * @param seed  seed to initialize {@code random} object
      */
     public RandomCompat(long seed) {
         this.random = new Random(seed);
@@ -57,18 +56,37 @@ public final class RandomCompat {
      * @param streamSize the number of values to generate
      * @return a stream of pseudorandom {@code int} values
      * @throws IllegalArgumentException if {@code streamSize} is
-     *          less than zero
+     *         less than zero
      */
     public IntStream ints(long streamSize) {
-
-        if(streamSize < 0L)
-            throw new IllegalArgumentException();
-
-        if(streamSize == 0L)
+        if (streamSize < 0L) throw new IllegalArgumentException();
+        if (streamSize == 0L) {
             return IntStream.empty();
-
+        }
         return ints().limit(streamSize);
     }
+
+    /**
+     * Returns a stream producing the given {@code streamSize} number of
+     * pseudorandom {@code double} values, each between zero (inclusive)
+     * and one (exclusive).
+     *
+     * <p>A pseudorandom {@code int} value is generated as if it's the result of
+     * calling the method {@link Random#nextDouble()}
+     *
+     * @param streamSize  the number of values to generate
+     * @return a stream of pseudorandom {@code double} values
+     * @throws IllegalArgumentException if {@code streamSize} is
+     *         less than zero
+     */
+    public DoubleStream doubles(long streamSize) {
+        if (streamSize < 0L) throw new IllegalArgumentException();
+        if (streamSize == 0L) {
+            return DoubleStream.empty();
+        }
+        return doubles().limit(streamSize);
+    }
+
 
     /**
      * Returns an effectively unlimited stream of pseudorandom {@code int}
@@ -89,45 +107,83 @@ public final class RandomCompat {
     }
 
     /**
+     * Returns an effectively unlimited stream of pseudorandom {@code double} values,
+     * each between zero (inclusive) and one (exclusive).
+     *
+     * <p>A pseudorandom {@code double} value is generated as if it's the result of
+     * calling the method {@link Random#nextDouble()}.
+     *
+     * @return a stream of pseudorandom {@code double} values
+     */
+    public DoubleStream doubles() {
+        return DoubleStream.generate(new DoubleSupplier() {
+            @Override
+            public double getAsDouble() {
+                return random.nextDouble();
+            }
+        });
+    }
+
+    /**
      * Returns a stream producing the given {@code streamSize} number
      * of pseudorandom {@code int} values, each conforming to the given
      * origin (inclusive) and bound (exclusive).
      *
      * @param streamSize the number of values to generate
-     * @param randomNumberOrigin the origin (inclusive) of each random value
-     * @param randomNumberBound the bound (exclusive) if each random value
+     * @param randomNumberOrigin  the origin (inclusive) of each random value
+     * @param randomNumberBound  the bound (exclusive) if each random value
      * @return a stream of pseudorandom {@code int} values,
-     *          each with the given origin (inclusive) and bound (exclusive)
+     *         each with the given origin (inclusive) and bound (exclusive)
      * @throws IllegalArgumentException if {@code streamSize} is
-     *          less than zero, or {@code randomNumberOrigin} is
-     *          greater than or equal to {@code randomNumberBound}
+     *         less than zero, or {@code randomNumberOrigin} is
+     *         greater than or equal to {@code randomNumberBound}
      */
     public IntStream ints(long streamSize, final int randomNumberOrigin, final int randomNumberBound) {
-        if(streamSize < 0L)
-            throw new IllegalArgumentException();
-
-        if(streamSize == 0L)
+        if (streamSize < 0L) throw new IllegalArgumentException();
+        if (streamSize == 0L) {
             return IntStream.empty();
-
+        }
         return ints(randomNumberOrigin, randomNumberBound).limit(streamSize);
+    }
+
+    /**
+     * Returns a stream producing the given {@code streamSize} number
+     * of pseudorandom {@code double} values, each conforming
+     * to the given origin (inclusive) and bound (exclusive).
+     *
+     * @param streamSize the number of values to generate
+     * @param randomNumberOrigin  the origin (inclusive) of each random value
+     * @param randomNumberBound  the bound (exclusive) if each random value
+     * @return a stream of pseudorandom {@code double} values,
+     *         each with the given origin (inclusive) and bound (exclusive)
+     * @throws IllegalArgumentException if {@code streamSize} is
+     *         less than zero, or {@code randomNumberOrigin} is
+     *         greater than or equal to {@code randomNumberBound}
+     */
+    public DoubleStream doubles(long streamSize,
+            final double randomNumberOrigin, final double randomNumberBound) {
+        if (streamSize < 0L) throw new IllegalArgumentException();
+        if (streamSize == 0L) {
+            return DoubleStream.empty();
+        }
+        return doubles(randomNumberOrigin, randomNumberBound).limit(streamSize);
     }
 
     /**
      * Returns an effectively unlimited stream of pseudorandom {@code int}
      * values, each conforming to the given origin (inclusive) and bound (exclusive)
      *
-     * @param randomNumberOrigin the origin (inclusive) of each random value
-     * @param randomNumberBound the bound (exclusive) of each random value
+     * @param randomNumberOrigin  the origin (inclusive) of each random value
+     * @param randomNumberBound  the bound (exclusive) of each random value
      * @return a stream of pseudorandom {@code int} values,
-     *          each with the given origin (inclusive) and bound (exclusive)
+     *         each with the given origin (inclusive) and bound (exclusive)
      * @throws IllegalArgumentException if {@code randomNumberOrigin}
-     *          is greater than or equal to {@code randomNumberBound}
+     *         is greater than or equal to {@code randomNumberBound}
      */
     public IntStream ints(final int randomNumberOrigin, final int randomNumberBound) {
-
-        if(randomNumberOrigin >= randomNumberBound)
+        if (randomNumberOrigin >= randomNumberBound) {
             throw new IllegalArgumentException();
-
+        }
         return IntStream.generate(new IntSupplier() {
 
             private final int bound = randomNumberBound - randomNumberOrigin;
@@ -135,6 +191,36 @@ public final class RandomCompat {
             @Override
             public int getAsInt() {
                 return randomNumberOrigin + random.nextInt(bound);
+            }
+        });
+    }
+
+    /**
+     * Returns an effectively unlimited stream of pseudorandom {@code double}
+     * values, each conforming to the given origin (inclusive) and bound (exclusive)
+     *
+     * @param randomNumberOrigin  the origin (inclusive) of each random value
+     * @param randomNumberBound  the bound (exclusive) of each random value
+     * @return a stream of pseudorandom {@code double} values,
+     *         each with the given origin (inclusive) and bound (exclusive)
+     * @throws IllegalArgumentException if {@code randomNumberOrigin}
+     *         is greater than or equal to {@code randomNumberBound}
+     */
+    public DoubleStream doubles(final double randomNumberOrigin, final double randomNumberBound) {
+        if (randomNumberOrigin >= randomNumberBound) {
+            throw new IllegalArgumentException();
+        }
+        return DoubleStream.generate(new DoubleSupplier() {
+
+            private final double bound = randomNumberBound - randomNumberOrigin;
+
+            @Override
+            public double getAsDouble() {
+                double result = random.nextDouble() * bound + randomNumberOrigin;
+                if (result >= randomNumberBound) {
+                    result = Double.longBitsToDouble(Double.doubleToLongBits(randomNumberBound) - 1);
+                }
+                return result;
             }
         });
     }
--- stream/src/test/java/com/annimon/stream/RandomCompatTest.java
+++ stream/src/test/java/com/annimon/stream/RandomCompatTest.java
@@ -1,13 +1,16 @@
 package com.annimon.stream;
 
+import com.annimon.stream.function.DoubleConsumer;
 import com.annimon.stream.function.IntConsumer;
 import java.security.SecureRandom;
+import java.util.Random;
 import org.junit.Test;
-
+import static org.hamcrest.Matchers.closeTo;
+import static org.hamcrest.Matchers.instanceOf;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
-import static org.hamcrest.Matchers.instanceOf;
+import static org.junit.Assert.fail;
 
 public class RandomCompatTest {
 
@@ -20,16 +23,30 @@ public class RandomCompatTest {
         assertThat(secureRandom.getRandom(), instanceOf(SecureRandom.class));
     }
 
+
     @Test
     public void testRandomIntsSized() {
         assertEquals(10, new RandomCompat().ints(10).count());
         assertEquals(0, new RandomCompat(1).ints(0).count());
     }
 
+    @Test
+    public void testRandomDoublesSized() {
+        assertEquals(10, new RandomCompat().doubles(10).count());
+        assertEquals(0, new RandomCompat(1).doubles(0).count());
+    }
+
+
     @Test(expected = IllegalArgumentException.class)
     public void testRandimIntsSizedNegative() {
         new RandomCompat().ints(-5);
     }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testRandimDoublesSizedNegative() {
+        new RandomCompat().doubles(-5);
+    }
+
 
     @Test
     public void testRandomInts() {
@@ -37,27 +54,81 @@ public class RandomCompatTest {
     }
 
     @Test
+    public void testRandomDoubles() {
+        assertEquals(15, new RandomCompat(1).doubles().skip(20).limit(15).count());
+    }
+
+    @Test
+    public void testRandomDoublesDefaultBound() {
+        new RandomCompat(1).doubles().peek(new DoubleConsumer() {
+            @Override
+            public void accept(double value) {
+                if (value < 0.0 || value >= 1.0)
+                    fail();
+            }
+        }).limit(20).count();
+    }
+
+    @Test
+    public void testRandomDoublesSameValues() {
+        final long seed = System.currentTimeMillis();
+        final Random random = new Random(seed);
+        new RandomCompat(new Random(seed)).doubles().peek(new DoubleConsumer() {
+            @Override
+            public void accept(double value) {
+                assertThat(value, closeTo(random.nextDouble(), 0.000000001));
+            }
+        }).limit(20).count();
+    }
+
+
+    @Test
     public void testRandomIntsSizedBounded() {
         assertEquals(20, new RandomCompat().ints(20, 5, 42).peek(new IntConsumer() {
             @Override
             public void accept(int value) {
                 if (value < 5 || value >= 42)
-                    throw new IllegalStateException();
+                    fail();
             }
         }).count());
 
         new RandomCompat(100).ints(0, 1, 20).forEach(new IntConsumer() {
             @Override
             public void accept(int value) {
-                throw new IllegalArgumentException();
+                fail();
             }
         });
     }
+
+    @Test
+    public void testRandomDoublesSizedBounded() {
+        assertEquals(20, new RandomCompat().doubles(20, -0.5, 4.2).peek(new DoubleConsumer() {
+            @Override
+            public void accept(double value) {
+                if (value < -0.5 || value >= 4.2)
+                    fail();
+            }
+        }).count());
+
+        new RandomCompat(100).doubles(0, 1d, 20d).forEach(new DoubleConsumer() {
+            @Override
+            public void accept(double value) {
+                fail();
+            }
+        });
+    }
+
 
     @Test(expected = IllegalArgumentException.class)
     public void testRandomIntsSizedBoundedNegative() {
         new RandomCompat().ints(-1, 0, 1);
     }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testRandomDoublesSizedBoundedNegative() {
+        new RandomCompat().doubles(-1, 0d, 1d);
+    }
+
 
     @Test
     public void testRandomIntsBounded() {
@@ -65,14 +136,39 @@ public class RandomCompatTest {
             @Override
             public void accept(int value) {
                 if (value < 0 || value > 99)
-                    throw new IllegalStateException();
+                    fail();
             }
         }).limit(20).skip(10).count());
     }
 
+    @Test
+    public void testRandomDoublesBounded() {
+        assertEquals(10, new RandomCompat().doubles(-1.19, -1.119).peek(new DoubleConsumer() {
+            @Override
+            public void accept(double value) {
+                if (value < -1.19 || value > -1.119)
+                    fail();
+            }
+        }).limit(20).skip(10).count());
+
+        new RandomCompat().doubles(0.001002003000, 0.001002003001).peek(new DoubleConsumer() {
+            @Override
+            public void accept(double value) {
+                if (value < 0.001002003000 || value > 0.001002003001)
+                    fail();
+            }
+        }).limit(20).count();
+    }
+
+
     @Test(expected = IllegalArgumentException.class)
     public void testRandomIntsBoundedInvalid() {
         new RandomCompat(111).ints(100, 100);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testRandomDoublesBoundedInvalid() {
+        new RandomCompat(111).doubles(10.0, 10.0);
     }
 }
 
