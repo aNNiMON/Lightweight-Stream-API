56197e6096cb4c2a10d1702da61f91d3d6f44e60
Victor <annimon119@gmail.com>
2017-02-20 21:13:32 +0200

    Add LongStream takeUntil operator

--- stream/src/test/java/com/annimon/stream/LongStreamTest.java
+++ stream/src/test/java/com/annimon/stream/LongStreamTest.java
@@ -451,6 +451,32 @@ public class LongStreamTest {
     }
 
     @Test
+    public void testTakeUntil() {
+        long[] expected = {2, 4, 6, 7};
+        long[] actual = LongStream.of(2, 4, 6, 7, 8, 10, 11)
+                .takeUntil(LongPredicate.Util.negate(Functions.remainderLong(2)))
+                .toArray();
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void testTakeUntilFirstMatch() {
+        long[] expected = {2};
+        long[] actual = LongStream.of(2, 4, 6, 7, 8, 10, 11)
+                .takeUntil(Functions.remainderLong(2))
+                .toArray();
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void testTakeUntilNoneMatch() {
+        long count = LongStream.of(2, 4, 6, 7, 8, 10, 11)
+                .takeUntil(Functions.remainderLong(128))
+                .count();
+        assertThat(count, is(7L));
+    }
+
+    @Test
     public void testDropWhile() {
         LongStream stream;
         stream = LongStream.of(12, 32, 22, 9, 30, 41, 42)
