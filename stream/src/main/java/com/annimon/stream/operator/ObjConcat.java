5d437beea9a2f9ee1391e01bfd0ba50a927c70d0
Victor <annimon119@gmail.com>
2017-02-22 22:33:57 +0200

    Reduce calls to first iterator hasNext method in ObjConcat

--- stream/src/main/java/com/annimon/stream/operator/ObjConcat.java
+++ stream/src/main/java/com/annimon/stream/operator/ObjConcat.java
@@ -7,18 +7,23 @@ public class ObjConcat<T> extends LsaExtIterator<T> {
 
     private final Iterator<? extends T> iterator1;
     private final Iterator<? extends T> iterator2;
+    private boolean firstStreamIsCurrent;
 
     public ObjConcat(Iterator<? extends T> iterator1, Iterator<? extends T> iterator2) {
         this.iterator1 = iterator1;
         this.iterator2 = iterator2;
+        firstStreamIsCurrent = true;
     }
 
     @Override
     protected void nextIteration() {
-        if (iterator1.hasNext()) {
-            next = iterator1.next();
-            hasNext = true;
-            return;
+        if (firstStreamIsCurrent) {
+            if (iterator1.hasNext()) {
+                next = iterator1.next();
+                hasNext = true;
+                return;
+            }
+            firstStreamIsCurrent = false;
         }
         if (iterator2.hasNext()) {
             next = iterator2.next();
