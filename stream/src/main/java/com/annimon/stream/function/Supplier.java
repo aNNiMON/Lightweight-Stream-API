e0327f5b152e73a6739f40e064c3686d002fc9d3
Victor <annimon119@gmail.com>
2017-03-09 11:45:29 +0200

    Add Util.safe for Supplier

--- stream/src/main/java/com/annimon/stream/function/Supplier.java
+++ stream/src/main/java/com/annimon/stream/function/Supplier.java
@@ -14,4 +14,50 @@ public interface Supplier<T> {
      * @return a result
      */
     T get();
+
+    class Util {
+
+        private Util() { }
+
+        /**
+         * Creates a safe {@code Supplier}.
+         *
+         * @param <T> the type of the result
+         * @param throwableSupplier  the supplier that may throw an exception
+         * @return a {@code Supplier}
+         * @throws NullPointerException if {@code throwableSupplier} is null
+         * @since 1.1.7
+         * @see #safe(com.annimon.stream.function.ThrowableSupplier, java.lang.Object)
+         */
+        public static <T> Supplier<T> safe(ThrowableSupplier<? extends T, Throwable> throwableSupplier) {
+            return Util.<T>safe(throwableSupplier, null);
+        }
+
+        /**
+         * Creates a safe {@code Supplier}.
+         *
+         * @param <T> the type of the result
+         * @param throwableSupplier  the supplier that may throw an exception
+         * @param resultIfFailed  the result which returned if exception was thrown
+         * @return a {@code Supplier}
+         * @throws NullPointerException if {@code throwableSupplier} is null
+         * @since 1.1.7
+         */
+        public static <T> Supplier<T> safe(
+                final ThrowableSupplier<? extends T, Throwable> throwableSupplier,
+                final T resultIfFailed) {
+            return new Supplier<T>() {
+
+                @Override
+                public T get() {
+                    try {
+                        return throwableSupplier.get();
+                    } catch (Throwable ex) {
+                        return resultIfFailed;
+                    }
+                }
+            };
+        }
+
+    }
 }
--- stream/src/test/java/com/annimon/stream/function/SupplierTest.java
+++ stream/src/test/java/com/annimon/stream/function/SupplierTest.java
@@ -1,17 +1,44 @@
 package com.annimon.stream.function;
 
 import com.annimon.stream.Functions;
-import static org.hamcrest.CoreMatchers.instanceOf;
-import static org.junit.Assert.*;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import static com.annimon.stream.test.hamcrest.CommonMatcher.hasOnlyPrivateConstructors;
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Tests {@code Supplier}.
- * 
+ *
  * @see com.annimon.stream.function.Supplier
  */
 public class SupplierTest {
-    
+
+    private static byte[] input;
+
+    @BeforeClass
+    public static void setUp() throws IOException {
+        final ByteArrayOutputStream baos = new ByteArrayOutputStream(15);
+        final DataOutputStream dos = new DataOutputStream(baos);
+        dos.writeUTF("test");
+        dos.writeUTF("123");
+        input = baos.toByteArray();
+    }
+
+    @Test
+    public void testPrivateConstructor() throws Exception {
+        assertThat(Supplier.Util.class, hasOnlyPrivateConstructors());
+    }
+
     @Test
     public void testGetString() {
         Supplier<String> supplier = new Supplier<String>() {
@@ -23,16 +50,16 @@ public class SupplierTest {
         };
         assertEquals("fantastic", supplier.get());
     }
-    
+
     @Test
     public void testGetStringBuilder() {
         Supplier<StringBuilder> supplier = Functions.stringBuilderSupplier();
-        
+
         assertThat(supplier.get(), instanceOf(StringBuilder.class));
         assertTrue(supplier.get().toString().isEmpty());
-        
+
     }
-    
+
     @Test
     public void testIncrement() {
         Supplier<Integer> supplier = new Supplier<Integer>() {
@@ -47,5 +74,41 @@ public class SupplierTest {
         assertEquals(1, supplier.get().intValue());
         assertEquals(2, supplier.get().intValue());
         assertEquals(3, supplier.get().intValue());
+    }
+
+    @Test
+    public void testSafe() throws IOException {
+        DataInputStream dis = new DataInputStream(new ByteArrayInputStream(input));
+
+        Supplier<String> supplier = Supplier.Util.safe(new UnsafeSupplier(dis));
+        assertThat(supplier.get(), is("test"));
+        assertThat(supplier.get(), is("123"));
+        assertThat(supplier.get(), is(nullValue()));
+        assertThat(supplier.get(), is(nullValue()));
+    }
+
+    @Test
+    public void testSafeWithOnFailedSupplier() throws IOException {
+        DataInputStream dis = new DataInputStream(new ByteArrayInputStream(input));
+
+        Supplier<String> supplier = Supplier.Util.safe(new UnsafeSupplier(dis), "oops");
+        assertThat(supplier.get(), is("test"));
+        assertThat(supplier.get(), is("123"));
+        assertThat(supplier.get(), is("oops"));
+        assertThat(supplier.get(), is("oops"));
+    }
+
+    private static class UnsafeSupplier implements ThrowableSupplier<String, Throwable> {
+
+        private final DataInputStream is;
+
+        public UnsafeSupplier(DataInputStream is) {
+            this.is = is;
+        }
+
+        @Override
+        public String get() throws IOException {
+            return is.readUTF();
+        }
     }
 }
