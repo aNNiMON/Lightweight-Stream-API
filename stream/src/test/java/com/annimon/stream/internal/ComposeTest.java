022894fc00842294324f17babebaf0f33a06f424
Victor <annimon119@gmail.com>
2017-04-22 13:03:37 +0300

    Ability to compose closeables

--- stream/src/test/java/com/annimon/stream/internal/ComposeTest.java
+++ stream/src/test/java/com/annimon/stream/internal/ComposeTest.java
@@ -1,7 +1,10 @@
 package com.annimon.stream.internal;
 
+import java.io.Closeable;
+import java.io.IOException;
 import org.junit.Test;
 import static com.annimon.stream.test.hamcrest.CommonMatcher.hasOnlyPrivateConstructors;
+import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
@@ -106,6 +109,154 @@ public class ComposeTest {
             composed.run();
         } catch (AssertionError error) {
             assertThat(error.getMessage(), is("1"));
+        }
+        assertThat(counter[0], is(2));
+    }
+
+
+    @Test
+    public void testComposeCloseables() {
+        final int[] counter = new int[] { 0 };
+        final Closeable closeable = new Closeable() {
+            @Override
+            public void close() throws IOException {
+                counter[0]++;
+            }
+        };
+
+        Runnable composed = Compose.closeables(closeable, closeable);
+        composed.run();
+        assertThat(counter[0], is(2));
+    }
+
+    @Test
+    public void testComposeCloseablesWithExceptionOnFirst() {
+        final int[] counter = new int[] { 0 };
+        final Closeable closeable1 = new Closeable() {
+            @Override
+            public void close() throws IOException {
+                counter[0]++;
+                throw new IllegalStateException("ok");
+            }
+        };
+        final Closeable closeable2 = new Closeable() {
+            @Override
+            public void close() throws IOException {
+                counter[0]++;
+            }
+        };
+
+        Runnable composed = Compose.closeables(closeable1, closeable2);
+        try {
+            composed.run();
+        } catch (IllegalStateException ex) {
+            assertThat(ex.getMessage(), is("ok"));
+        }
+        assertThat(counter[0], is(2));
+    }
+
+    @Test
+    public void testComposeCloseablesWithExceptionOnSecond() {
+        final int[] counter = new int[] { 0 };
+        final Closeable closeable1 = new Closeable() {
+            @Override
+            public void close() throws IOException {
+                counter[0]++;
+            }
+        };
+        final Closeable closeable2 = new Closeable() {
+            @Override
+            public void close() throws IOException {
+                counter[0]++;
+                throw new IllegalArgumentException("ok");
+            }
+        };
+
+        Runnable composed = Compose.closeables(closeable1, closeable2);
+        try {
+            composed.run();
+        } catch (IllegalArgumentException ex) {
+            assertThat(ex.getMessage(), is("ok"));
+        }
+        assertThat(counter[0], is(2));
+    }
+
+    @Test
+    public void testComposeCloseablesWithWrappedExceptionOnSecond() {
+        final int[] counter = new int[] { 0 };
+        final Closeable closeable1 = new Closeable() {
+            @Override
+            public void close() throws IOException {
+                counter[0]++;
+            }
+        };
+        final Closeable closeable2 = new Closeable() {
+            @Override
+            public void close() throws IOException {
+                counter[0]++;
+                throw new IOException("ok");
+            }
+        };
+
+        Runnable composed = Compose.closeables(closeable1, closeable2);
+        try {
+            composed.run();
+        } catch (RuntimeException ex) {
+            assertThat(ex.getCause(), instanceOf(IOException.class));
+            assertThat(ex.getCause().getMessage(), is("ok"));
+        }
+        assertThat(counter[0], is(2));
+    }
+
+    @Test
+    public void testComposeCloseablesWithError() {
+        final int[] counter = new int[] { 0 };
+        final Closeable closeable1 = new Closeable() {
+            @Override
+            public void close() throws IOException {
+                counter[0]++;
+                throw new AssertionError("1");
+            }
+        };
+        final Closeable closeable2 = new Closeable() {
+            @Override
+            public void close() throws IOException {
+                counter[0]++;
+                throw new AssertionError("2");
+            }
+        };
+
+        Runnable composed = Compose.closeables(closeable1, closeable2);
+        try {
+            composed.run();
+        } catch (AssertionError error) {
+            assertThat(error.getMessage(), is("1"));
+        }
+        assertThat(counter[0], is(2));
+    }
+
+    @Test
+    public void testComposeCloseablesWithErrorOnSecond() {
+        final int[] counter = new int[] { 0 };
+        final Closeable closeable1 = new Closeable() {
+            @Override
+            public void close() throws IOException {
+                counter[0]++;
+            }
+        };
+        final Closeable closeable2 = new Closeable() {
+            @Override
+            public void close() throws IOException {
+                counter[0]++;
+                throw new AssertionError("ok");
+            }
+        };
+
+        Runnable composed = Compose.closeables(closeable1, closeable2);
+        try {
+            composed.run();
+        } catch (AssertionError ex) {
+            assertThat(ex.getMessage(), is("ok"));
         }
         assertThat(counter[0], is(2));
     }
