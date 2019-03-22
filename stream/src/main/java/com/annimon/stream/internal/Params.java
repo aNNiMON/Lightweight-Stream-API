f29fd57208c20252a4549b084d55ed082c3e58f0
Victor <annimon119@gmail.com>
2019-03-22 23:39:00 +0200

    Commit forgotten file

--- stream/src/main/java/com/annimon/stream/internal/Params.java
+++ stream/src/main/java/com/annimon/stream/internal/Params.java
@@ -1,9 +1,24 @@
 package com.annimon.stream.internal;
 
+import org.jetbrains.annotations.NotNull;
+
 /**
  * Parameters for streams.
  */
 public final class Params {
 
     public Runnable closeHandler;
+
+    public static Params wrapWithCloseHandler(Params params, @NotNull Runnable closeHandler) {
+        final Params newParams;
+        if (params == null) {
+            newParams = new Params();
+            newParams.closeHandler = closeHandler;
+        } else {
+            newParams = params;
+            final Runnable firstHandler = newParams.closeHandler;
+            newParams.closeHandler = Compose.runnables(firstHandler, closeHandler);
+        }
+        return newParams;
+    }
 }
