d72039bd4b5abef3b30ea6c235851767aca56a43
Victor <annimon119@gmail.com>
2020-10-21 21:23:30 +0300

    Add an ability to concat 3 or more Object streams and iterators

--- stream/src/main/java/com/annimon/stream/internal/Compose.java
+++ stream/src/main/java/com/annimon/stream/internal/Compose.java
@@ -1,6 +1,8 @@
 package com.annimon.stream.internal;
 
 import java.io.Closeable;
+import java.util.Iterator;
+import java.util.List;
 
 public final class Compose {
 
@@ -39,6 +41,28 @@ public final class Compose {
                     b.close();
                 } catch (Throwable e2) {
                     handleException(e2);
+                }
+            }
+        };
+    }
+
+    public static Runnable closeables(final List<? extends Closeable> closeables) {
+        return new Runnable() {
+            @Override
+            public void run() {
+                final Iterator<? extends Closeable> iterator = closeables.iterator();
+                while (iterator.hasNext()) {
+                    try {
+                        iterator.next().close();
+                    } catch (Throwable currentExc) {
+                        // close next closeables ignoring possible exceptions
+                        while (iterator.hasNext()) {
+                            try {
+                                iterator.next().close();
+                            } catch (Throwable ignore) { }
+                        }
+                        handleException(currentExc);
+                    }
                 }
             }
         };
--- stream/src/main/java/com/annimon/stream/operator/ObjConcat.java
+++ stream/src/main/java/com/annimon/stream/operator/ObjConcat.java
@@ -1,37 +1,42 @@
 package com.annimon.stream.operator;
 
 import com.annimon.stream.iterator.LsaExtIterator;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
+import java.util.List;
 import org.jetbrains.annotations.NotNull;
 
 public class ObjConcat<T> extends LsaExtIterator<T> {
 
-    private final Iterator<? extends T> iterator1;
-    private final Iterator<? extends T> iterator2;
-    private boolean firstStreamIsCurrent;
+    private final List<? extends Iterator<? extends T>> iterators;
+    private final int iteratorsCount;
+    private int iteratorIndex;
 
     public ObjConcat(
             @NotNull Iterator<? extends T> iterator1,
             @NotNull Iterator<? extends T> iterator2) {
-        this.iterator1 = iterator1;
-        this.iterator2 = iterator2;
-        firstStreamIsCurrent = true;
+        iterators = Arrays.asList(iterator1, iterator2);
+        iteratorsCount = 2;
+        iteratorIndex = 0;
+    }
+
+    public ObjConcat(List<? extends Iterator<? extends T>> iterators) {
+        this.iterators = new ArrayList<Iterator<? extends T>>(iterators);
+        iteratorsCount = iterators.size();
+        iteratorIndex = 0;
     }
 
     @Override
     protected void nextIteration() {
-        if (firstStreamIsCurrent) {
-            if (iterator1.hasNext()) {
-                next = iterator1.next();
+        while (iteratorIndex < iteratorsCount) {
+            Iterator<? extends T> currentIterator = iterators.get(iteratorIndex);
+            if (currentIterator.hasNext()) {
+                next = currentIterator.next();
                 hasNext = true;
                 return;
             }
-            firstStreamIsCurrent = false;
-        }
-        if (iterator2.hasNext()) {
-            next = iterator2.next();
-            hasNext = true;
-            return;
+            iteratorIndex++;
         }
         hasNext = false;
     }
